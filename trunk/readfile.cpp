79ed2a120d5176fd6490dffbfb99c66594717767
pscher1@umbc.edu <pscher1@umbc.edu@6f76e51c-30dc-c085-495d-2e1e1fe3654a>
2013-04-20 20:36:47 +0000

    Created map file specification and a simple class to read it in

---
+++ trunk/readfile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "world.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    World w("simple.map");
+
+    cout << "Loaded simple.map" << endl;
+    cout << "Size: " << w.getsizeX() << "," << w.getsizeY() << endl;
+    cout << "Start: " << w.getstartX() << "," << w.getstartY() << endl;
+    cout << "Goal: " << w.getgoalX() << "," << w.getgoalY() << endl;
+}
